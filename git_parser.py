59ba24d626ca952041d8adc7207542f903cbe160
dihadi-mantri <modulusfunctio9@gmail.com>
2026-04-05 14:17:40 +0530

    feat: add get_last_commit() to git_parser

--- git_parser.py
+++ git_parser.py
@@ -1,4 +1,5 @@
 """Git repository data parser using subprocess and gitpython."""
+from datetime import datetime, timezone
 from pathlib import Path
 
 from git import Repo
@@ -28,3 +29,35 @@ class GitParser:
         except Exception:
             head = self.repo.head.commit
             return f"detached ({head.hexsha[:7]})"
+
+    def _format_age(self, dt: datetime) -> str:
+        """Format a datetime as relative age string."""
+        now = datetime.now(timezone.utc)
+        if dt.tzinfo is None:
+            dt = dt.replace(tzinfo=timezone.utc)
+        delta = now - dt
+        seconds = int(delta.total_seconds())
+        if seconds < 60:
+            return f"{seconds}s ago"
+        elif seconds < 3600:
+            return f"{seconds // 60}m ago"
+        elif seconds < 86400:
+            return f"{seconds // 3600}h ago"
+        elif seconds < 604800:
+            return f"{seconds // 86400}d ago"
+        else:
+            return f"{seconds // 604800}w ago"
+
+    def get_last_commit(self) -> dict:
+        """Get info about the most recent commit."""
+        if not self.is_valid_repo():
+            return {"hash": "", "message": "", "author": "", "time": ""}
+        commit = self.repo.head.commit
+        committed_date = commit.committed_datetime
+        age = self._format_age(committed_date)
+        return {
+            "hash": commit.hexsha[:7],
+            "message": commit.message.strip().split("\n")[0],
+            "author": str(commit.author),
+            "time": age,
+        }
